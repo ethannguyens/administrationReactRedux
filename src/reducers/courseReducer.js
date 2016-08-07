89c94fc7b55d833437109839eee7e18e51fcad92
Ethan <ethan.nguyens@gmail.com>
2016-08-07 12:45:51 +0100

    add mock api

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,11 @@
 import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
-  switch(action.type) {
+  switch (action.type) {
     case types.CREATE_COURSE:
-        return [...state,
+      return [...state,
         Object.assign({}, action.course)
-        ];
+      ];
 
     default:
       return state;
