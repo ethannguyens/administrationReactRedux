ddda621bad9fd719f43a9eb3244da44e607304a7
Ethan <ethan.nguyens@gmail.com>
2016-08-07 12:21:55 +0100

    Add bindActionsCreators and Action Type Constants

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css'; //webpack import css
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
