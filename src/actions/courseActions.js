30fb990c241597ee3d59c5d536f3b1009cc12280
Ethan <ethan.nguyens@gmail.com>
2016-08-07 19:13:02 +0100

    action and reducer to load courses from api

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,17 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mochCourseApi';
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course};
+export function loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, loadCoursesSuccess};
+}
+
+//thunk
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(err => {
+      throw(err);
+    });
+  }
 }
