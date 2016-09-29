61973112e46c9f8f127eb2fde0431a5b9feb9b3b
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-09-29 14:28:42 +0100

    add CourseForm component

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,24 +2,34 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourceForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
-      <h1> Manage Course</h1>
+      <div>
+        <h1> Manage Course</h1>
+        <CourseForm course={this.state.course}></CourseForm>
+      </div>
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-  //myprop: PropTypes.string.isRequired
+  //
 };
 
 function mapStateToProps(state, ownProps) {
+  let course = {id: '', watchHref: '', title: '', length: '', category: ''};
   return {
     state: state
   };
